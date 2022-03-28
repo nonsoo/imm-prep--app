04c82407f304525d05d52d7becdb714ad42d73b2
Nonso <nonsootoh@gmail.com>
2022-03-28 15:06:22 -0400

    adds gAnalytics

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import "./css/QuestionComp.css";
 import "./css/loading.css";
 
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import ReactGA from "react-ga";
 
 import Topics from "./Components/Topics";
 import QuestionComp from "./Components/QuestionComp";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(process.env.REACT_APP_GA_ID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <div className="Question-Site">
       <Routes>
